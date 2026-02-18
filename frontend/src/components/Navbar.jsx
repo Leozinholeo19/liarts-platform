64446230ee04c0c0ecaf185ccbcb90d5b9b24923
Leozinholeo19 <leonardomachado310@gmail.com>
2026-02-18 14:57:40 -0300

    Adicionando contador de itens no carrinho na navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CarrinhoContext } from "../context/CarrinhoContext";
 import "../styles/navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/produtos">Produtos</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/cadastro">Cadastro</Link>
-          <Link to="/carrinho">Carrinho</Link>
-        </nav>
-      );
+  const { carrinho } = useContext(CarrinhoContext);
+
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/produtos">Produtos</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/cadastro">Cadastro</Link>
+      <Link to="/carrinho">
+        Carrinho ({carrinho.length})
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
