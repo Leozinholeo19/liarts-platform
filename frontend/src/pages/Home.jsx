5b7b32a93ba7347e79100ac632599365664a9ae9
Leozinholeo19 <leonardomachado310@gmail.com>
2026-02-18 15:13:20 -0300

    Criando layout inicial da home com identidade visual

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
+import "../styles/home.css";
+
 function Home() {
-    return (
-      <div>
-        <h1>Bem-vindo à LiArts</h1>
+  return (
+    <div className="home-container">
+      <div className="hero">
+        <h1>Bem-vindo(a) à LiArts Miçangas</h1>
+
+        <p>
+          Descubra um mundo encantador de arte e criatividade com nossos
+          produtos feitos à mão. Criamos peças exclusivas com dedicação,
+          carinho e atenção aos detalhes.
+        </p>
+
+        <Link to="/produtos">
+          <button className="botao-principal">
+            Ver Produtos
+          </button>
+        </Link>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+    </div>
+  );
+}
+
+export default Home;
